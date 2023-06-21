b3b95b731a9be5bd95f8546a0b0fb8aefc65747b
Poodle Moth <110733000+WhitePoodleMoth@users.noreply.github.com>
2023-06-21 13:14:10 -0700

    Fix API
    * Update localhost address
    * Update API headers

--- notfound/lib/api.dart
+++ notfound/lib/api.dart
@@ -1,18 +1,20 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-const String endSearch = "http://127.0.0.1:8000/api/v1/search/";
+const String endSearch = "http://10.0.2.2:8000/api/v1/search/";
 
 Future<Map<String, dynamic>?> searchRa(String ra) async {
   try {
     var response = await http.post(
       Uri.parse(endSearch),
-      body: {"ra": ra},
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({"ra": ra}),
     );
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
     }
-  } catch (_) {
+  } catch (e) {
+    print('Ocorreu um erro: $e');
     return null;
   }
   return null;
